5636625ada131b1b6b5dbbfbd5179b0bf635fd44
SEOKWOOPARK <skkfea08@gmail.com>
2025-08-24 02:01:03 -0400

    Update associative/unordered containers examples

--- Standard_Library/std_associative_containers.cpp
+++ Standard_Library/std_associative_containers.cpp
@@ -1,3 +1,83 @@
-//
-// Created by SEOKWOO PARK on 8/21/25.
-//
+#include <iostream>
+#include <set>
+#include <map>
+
+using namespace std;
+
+// Associative container: Automatically sorted, balanced tree-based, Red - black tree
+// Implemented with a balanced binary search tree
+// Lookup, insertion, deletion -> Average O(log N)
+
+int main() {
+    // Set -> Removing duplicate values and leaving just one unique data
+    set<int> s1 = {1, 2, 3, 3, 3, 3, 3};
+
+    for (int n: s1) {
+        cout << n << " ";
+    }
+
+    cout << endl;
+
+    s1.insert(10);
+    s1.insert(10);
+
+    for (int n: s1) {
+        cout << n << " ";
+    }
+
+    cout << endl;
+    cout << "----------------------" << endl;
+
+    // Multiset: Allowing duplicates
+    multiset<int> s2 = {1, 2, 3, 3, 3, 3, 3};
+    s2.insert(10);
+    s2.insert(10);
+
+    for (int n: s2) {
+        cout << n << " ";
+    }
+
+    cout << endl;
+    cout << "----------------------" << endl;
+
+    // Map -> (Key, value) & sorted by each key
+    map<string, int> m1 = {{"one", 1}, {"two", 2}};
+    m1["three"] = 3;
+    m1["four"] = 4;
+    m1.insert({"five", 5});
+
+    for (const auto& pair: m1) {
+        cout << pair.first << " = " << pair.second << endl;
+    }
+
+    m1.erase("three");
+
+    cout << "Deleting" << endl;
+
+    for (const auto& pair: m1) {
+        cout << pair.first << " = " << pair.second << endl;
+    }
+
+    cout << "----------------------" << endl;
+
+    // Multimap: Allowing duplicate keys
+    multimap<string, int> m2 = {{"one", 1}, {"two", 2}};
+    m2.insert({"one", 1});
+    m2.insert({"one", 11});
+    m2.insert({"two", 2});
+    m2.insert({"two", 22});
+
+    for (const auto& pair: m2) {
+        cout << pair.first << " = " << pair.second << endl;
+    }
+
+    cout << "After deleting 'one' key" << endl;
+
+    m2.erase("one");
+
+    for (const auto& pair: m2) {
+        cout << pair.first << " = " << pair.second << endl;
+    }
+
+    return 0;
+}
--- Standard_Library/std_unordered_containers.cpp
+++ Standard_Library/std_unordered_containers.cpp
@@ -1,3 +1,32 @@
-//
-// Created by SEOKWOO PARK on 8/24/25.
-//
+#include <iostream>
+#include <unordered_set>
+#include <unordered_map>
+
+using namespace std;
+
+// Unlike map or set, unordered containers use a hash table internally
+// Not keeping elements sorted
+// Operation -> O(1) on average and O(n) on the worst collision case
+// Slightly higher memory overhead (because of hashing buckets).
+int main() {
+
+   unordered_set<string> s1 = {"one", "three", "four", "four", "two"};
+
+   for (string x: s1) {
+      cout << x << " ";
+   }
+
+   cout << endl;
+   cout << "----------------------" << endl;
+
+   unordered_map<string, int> m1 = {{"one", 1}, {"two", 2}};
+   m1["five"] = 5;
+   m1["five"] = 5;
+   m1.insert({"six", 6});
+
+   for (const auto& pair: m1) {
+      cout << pair.first << " " << pair.second << endl;
+   }
+
+   return 0;
+}
