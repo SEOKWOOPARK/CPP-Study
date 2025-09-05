5ad0ae04d8b0d02b878b4e40ac21944811654521
SEOKWOOPARK <skkfea08@gmail.com>
2025-09-05 15:37:10 -0400

    Template Alias: Default template args by Map

--- modern_feature/template_3.cpp
+++ modern_feature/template_3.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <Eigen/Dense>
+#include <map>
 using namespace Eigen;
 using namespace std;
 
@@ -17,16 +18,31 @@ using vec_size_3 = array<T, 3>;
 using Mat3d = Eigen::Matrix<double, 3, 3>;
 using Vec3d = Eigen::Matrix<double, 3, 1>;
 
+// Template Alias3: Default template args
+template <typename T>
+using StringMap = map<string, T>;
+
 int main() {
     printAll("Hello World! ", 3, " ", true, " ", 3.5);
 
     vec_size_3<float> point = {1.1, 2.2, 3.3};
     cout << point[0] << " " << point[1] << " " << point[2] << endl;
+    cout << "---------------------" << endl;
 
     Vec3d v(1, 2, 3);
     Mat3d I = Mat3d::Identity();
     cout << "I equals to \n" << I << endl;
     cout << "v equals to \n" << v << endl;
+
+    cout << "---------------------" << endl;
+    StringMap<int> ages;
+    ages["John"] = 50;
+    ages["Ali"] = 30;
+    ages["Chris"] = 400;
+
+    for (auto& [key, value]: ages) {
+        cout << key << ": " << value << endl;
+    }
 
 }
 
