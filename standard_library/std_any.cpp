a9ddc1c9497f2de6c59ca91cde362916bbb5d3ba
SEOKWOOPARK <skkfea08@gmail.com>
2025-09-23 23:16:55 -0400

    Comment and Reference: any, any_cast

--- standard_library/std_any.cpp
+++ standard_library/std_any.cpp
@@ -4,15 +4,18 @@
 
 using namespace std;
 
+// 'any' since C++17: Useful when the type is not known at compile time
+// Throws std::bad_any_cast when it's a wrong type
+
 int main() {
 
-    any a = 42;
-    cout << any_cast<int>(a) << endl;
+    any a = 42; // A type-safe box that can hold one value of any copy-constructible type(a new object by copying an existing one)
+    cout << any_cast<int>(a) << endl; // Extracting the value as as int. If the held type is not int(meaning current type), it throws bad_any_cast
 
     a = string("Hello World!");
     cout << any_cast<string>(a) << endl;
 
-    if (a.type() == typeid(int)) {
+    if (a.type() == typeid(string)) {
         cout << "string type" << endl;
     }
 
