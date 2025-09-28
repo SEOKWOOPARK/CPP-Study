bafb5720c1b396f557106d2baa1a48c24e79e809
SEOKWOOPARK <skkfea08@gmail.com>
2025-09-28 01:04:36 -0400

    Comment for each line

--- concurrency/mutex_1.cpp
+++ concurrency/mutex_1.cpp
@@ -2,20 +2,36 @@
 #include <thread>
 #include <mutex>
 
+// Mutex: Synchronization primitive used to prevent multiple threads from accessing shared resources at the same time
+// which could otherwise lead to race conditions and unpredictable behavior
+
+// Race condition: Two or more threads(processes) access the same shared resource simultaneously.
+// The final outcome depends on the timing or interleaving of their operations
+
+
 using namespace std;
 
-int counter = 0;
+int counter = 0; // A shared variable between all threads
+mutex mtx; // Mmutex that will make sure only one thread at a time can change counter.
 
 void increment(int id) {
     for (int i = 0; i < 10; i++) {
+        // lock on entry + unlock on exit
+        // If another thread is already holding the lock, the current thread waits
+        // When the lock object goes out of 'for' scope, it automatically unlocks the mutex(RAII pattern)
         lock_guard<mutex> lock(mtx);
-        ++counter;
+
+        ++counter; // critical section: where shared resources are accessed or modified -> only one thread should execute it at once to avoid race conditions
     }
 }
 
-int main() {
-    jthread t1(increment, 1);
-    jthread t2(increment, 2);
+int main() { // The thread for main works when compiling and build is over
+    // Since each increment is protected, no two threads can increment 'counter' at the exact same time
+    thread t1(increment, 1); // thread 1 is running
+    thread t2(increment, 2); // thread 2 is running
+
+    t1.join(); // The thread for main waits for t1 thread until t1 is completely done
+    t2.join(); // The thread for main waits for t2 thread until t2 is completely done
 
     cout << "Final counter = " << counter << endl;
 }
