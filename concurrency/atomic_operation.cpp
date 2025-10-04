75564d6c1e4d27ea2e6d299fd47b3bec99dcf825
SEOKWOOPARK <skkfea08@gmail.com>
2025-10-04 17:53:51 -0400

    References for atomic operation

--- concurrency/atomic_operation.cpp
+++ concurrency/atomic_operation.cpp
@@ -7,6 +7,9 @@ using namespace std;
 
 // Atomic operation without Mutex
 // Testing whether multiple threads can safely and correctly increment a shared 'counter variable' exactly 2M times total without race conditions
+// Reference1 (compare_exchange_weak): https://en.cppreference.com/w/cpp/atomic/atomic_compare_exchange.html
+// Reference2( memory_order): https://en.cppreference.com/w/cpp/atomic/memory_order.html
+// Reference3 (Atomics in C++): https://ryonaldteofilo.medium.com/atomics-in-c-compare-and-swap-and-memory-order-part-2-64e127847e00
 
 // atomic integer -> Not interleaving from other threads
 atomic<int> counter(0);
@@ -20,8 +23,10 @@ void cas_increment(int n) {
         int old = counter.load(memory_order_relaxed);
 
         // compare_exchange_weak(expected, desired, ...)
+        // (Success in old == counter) memory_order_acq_rel = Release + Acquire
+        // (Fail in old == counter) When CAS fails, nothing changed—there’s no state transition to synchronize.
         while (!counter.compare_exchange_weak(old, old + 1, memory_order_acq_rel, memory_order_relaxed))
-        {}
+        {} // Empty body -> Retry loop. When CAS fails, nothing to do any extra work inside the loop
     }
 }
 
