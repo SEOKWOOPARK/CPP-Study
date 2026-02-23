d408a64830ff2bf03a2c6e98fd0f833a4b1c1be0
SEOKWOOPARK <skkfea08@gmail.com>
2026-02-22 23:22:08 -0500

    Comments for median_filter.cppe

--- OpenCV/median_filter.cpp
+++ OpenCV/median_filter.cpp
@@ -6,8 +6,11 @@
 using namespace std;
 
 // Reference: https://docs.opencv.org/3.4/d4/d86/group__imgproc__filter.html#ga564869aa33e58769b4469101aac458f9
+// Reference for vector.begin() https://cplusplus.com/reference/vector/vector/begin/
 
 int main() {
+    // cv::Mat stores image data as a flat memory block internally, not a 2D array.
+    //
     cv::Mat src = cv::imread("input.jpg", cv::IMREAD_GRAYSCALE);
 
     if (src.empty()) {
@@ -15,7 +18,7 @@ int main() {
         return -1;
     }
 
-    int k_size = 5;
+    int k_size = 5; // kernel size
 
     // ------------------------------------------------------------
 
@@ -33,23 +36,32 @@ int main() {
     int radius = k_size / 2;
     cv::Mat destination2 = cv::Mat::zeros(src.size(), CV_8UC1);
     vector<uchar> window;
-    window.reserve(k_size * k_size);
+    window.reserve(k_size * k_size); // Pre-allocating the capacity of the vector)
 
+    // Loops over every pixel, skipping borders -> y and x start at radius
     for (int y = radius; y < src.rows - radius; y++) {
         uchar* out = destination2.ptr<uchar>(y);
 
         for (int x = radius; x < src.cols - radius; x++) {
             window.clear();
 
-            for (int dy = -radius; dy < radius; dy++) {
+            // Collecting neighborhood pixels
+            for (int dy = -radius; dy <= radius; dy++) { // dy = -1, 0, 1   (rows:   above, center, below)
+                // When 'src.at<uchar>(y, x)' is used, OpenCV calculates the position every time -> Too slow
                 const uchar* row = src.ptr<uchar>(y + dy);
-                for (int dx = -radius; dx < radius; dx++) {
+
+                for (int dx = -radius; dx <= radius; dx++) { // dx = -1, 0, 1   (cols:   left, center, right)
                     window.push_back(row[x + dx]);
                 }
             }
 
-            auto middle = window.begin() + window.size() / 2;
+            // window.begin() returns an iterator(vector<uchar>::iterator) to the start of the vector
+            // window.size() returns just integer
+            vector<uchar>::iterator middle = window.begin() + window.size() / 2;
+            // nth_element partially sorts the vector so that the middle position has the correct median value.
             nth_element(window.begin(), middle, window.end());
+            // middle is an iterator (like a pointer) pointing to the median element inside window vector.
+            // To get the actual value it points to, dereferencing it with *.
             out[x] = *middle;
         }
     }
