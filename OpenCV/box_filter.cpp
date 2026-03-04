9c11a480258bdc4ca2fa5632fbc64a30f3f09071
SEOKWOOPARK <skkfea08@gmail.com>
2026-03-04 01:17:19 -0500

    Manual implmentation for Box Filter

--- OpenCV/box_filter.cpp
+++ OpenCV/box_filter.cpp
@@ -1,3 +1,26 @@
-//
-// Created by SEOKWOO PARK on 2/22/26.
-//
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main() {
+    cv::Mat src = cv::imread("input.jpg", cv::IMREAD_GRAYSCALE);
+
+    if (src.empty()) {
+        cout << "Could not open .jpg" << endl;
+        return -1;
+    }
+
+    Mat destination;
+    boxFilter(src, destination, -1, Size(5, 5));
+
+    imshow("src", src);
+    imshow("box filter", destination);
+    imwrite("box_filter.jpg", destination);
+
+    waitKey(0);
+    destroyAllWindows();
+
+    return 0;
+}
