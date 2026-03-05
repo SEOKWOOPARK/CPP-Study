560efda25e5b470c5ca2d15ff46a0b00d50833be
SEOKWOOPARK <skkfea08@gmail.com>
2026-03-04 23:18:38 -0500

    Built-in for Bilateral Filter

--- OpenCV/bilateral_filter.cpp
+++ OpenCV/bilateral_filter.cpp
@@ -1,3 +1,26 @@
-//
-// Created by SEOKWOO PARK on 2/22/26.
-//
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+int main() {
+
+    Mat src = imread("input.jpg", IMREAD_GRAYSCALE);
+
+    if (src.empty()) {
+        cout << "Could not open .jpg" << endl;
+        return -1;
+    }
+
+    Mat destination;
+    bilateralFilter(src, destination, 9, 75, 75);
+
+    imshow("src", src);
+    imshow("bilateral filter", destination);
+    imwrite("bilateral_filter.png", destination);
+
+    waitKey(0);
+    destroyAllWindows();
+    return 0;
+}
