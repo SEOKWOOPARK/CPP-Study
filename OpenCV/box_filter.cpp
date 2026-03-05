df797eb4472e4abe464624d234e18f0e65354960
SEOKWOOPARK <skkfea08@gmail.com>
2026-03-04 22:36:47 -0500

    Manual implmentation for Box Filter

--- OpenCV/box_filter.cpp
+++ OpenCV/box_filter.cpp
@@ -4,8 +4,34 @@
 using namespace cv;
 using namespace std;
 
+Mat manualBoxFilter(const Mat& src) {
+    int kernel_size = 5;
+    int padding = kernel_size / 2;
+    float weight = 1.0f / float(kernel_size * kernel_size);
+
+    Mat padded;
+    copyMakeBorder(src, padded, padding, padding, padding, padding, BORDER_DEFAULT);
+    Mat destination = Mat::zeros(src.rows, src.cols, CV_8U);
+
+    for (int y = 0; y < src.rows; y++) {
+        for (int x = 0; x < src.cols; x++) {
+            float sum = 0.0f;
+
+            for (int ky = 0; ky < kernel_size; ky++) {
+                for (int kx = 0; kx < kernel_size; kx++) {
+                    sum += padded.at<uchar>(y + ky, x + kx) * weight;
+                }
+            }
+
+            destination.at<uchar>(y, x) = saturate_cast<uchar>(sum);
+        }
+    }
+
+    return destination;
+}
+
 int main() {
-    cv::Mat src = cv::imread("input.jpg", cv::IMREAD_GRAYSCALE);
+    Mat src = imread("input.jpg", IMREAD_GRAYSCALE);
 
     if (src.empty()) {
         cout << "Could not open .jpg" << endl;
@@ -19,6 +45,16 @@ int main() {
     imshow("box filter", destination);
     imwrite("box_filter.jpg", destination);
 
+    waitKey(0);
+    destroyAllWindows();
+
+    // ----------------------------------------------------------------
+
+    Mat manual_destination = manualBoxFilter(src);
+
+    imshow("src", src);
+    imshow("manual box filter", manual_destination);
+    imwrite("manual box filter.png", manual_destination);
     waitKey(0);
     destroyAllWindows();
 
