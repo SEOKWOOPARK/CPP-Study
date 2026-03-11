9f76d0d6a34497f1913779e11af97172f8a132ab
SEOKWOOPARK <skkfea08@gmail.com>
2026-03-11 01:22:06 -0400

    Implementation for Memory Allocator (stage 2) - Attaching a header to every memory block

--- system/memory_allocator.cpp
+++ system/memory_allocator.cpp
@@ -14,16 +14,29 @@ using namespace std;
 void* heap_start = nullptr;
 void* heap_end = nullptr;
 
+struct BlockHeader {
+    size_t size; // 8 bytes
+    bool free; // 1 byte
+    // padding 7 bytes since compiler adds 7 bytes for alignemnt
+};
+
 void* extend_heap(size_t size) {
 
-    // sbrk: Returning the program break address before extending the memory
+    // sbrk(size): Returning the program break address before extending the memory as much as 'size'
     if (heap_start == nullptr) {
         heap_start = sbrk(0);
     }
 
-    void* prev_break = sbrk(size);
+    // prev_break (old break = start of new block = start of header)
+    void* prev_break = sbrk(size + sizeof(BlockHeader));
 
-    return prev_break;
+    BlockHeader* header = (BlockHeader*)prev_break; // casting
+    header->size = size;
+    header->free = false;
+
+    heap_end = sbrk(0);
+
+    return (void*)(header + 1);
 }
 
 
@@ -32,10 +45,20 @@ int main() {
     void* p1 = extend_heap(1024);
     void* p2 = extend_heap(512);
 
-    assert((char*)p2 - (char*)p1 == 1024);
+    assert((char*)p2 - (char*)p1 == 1040);
 
     cout << "p1: " << p1 << endl;
     cout << "p2: " << p2 << endl;
+
+    cout << "----------------------------" << endl;
+
+    BlockHeader* header = (BlockHeader*)heap_start;
+
+    assert(header->size == 1024);
+    assert(header->free == false);
+
+    cout << "Header is assigned" << endl;
+
 
     return 0;
 }
