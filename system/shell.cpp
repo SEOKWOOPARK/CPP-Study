81b4942e8bb89689212cc070aa2090a7c518ab4f
SEOKWOOPARK <skkfea08@gmail.com>
2026-03-24 00:33:34 -0400

    Stage 1 for Unix Shell - Basic command execution

---
+++ system/shell.cpp
@@ -0,0 +1,51 @@
+#include <unistd.h>
+#include <sys/wait.h>
+#include <vector>
+#include <sstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    while (true) {
+        cout << "$ " << flush;
+
+        string line;
+
+        if (!getline(cin,line)) {
+            break;
+        }
+
+        vector<string> tokens;
+        istringstream ss(line);
+        string token;
+
+        while (ss >> token) {
+            tokens.push_back(token);
+        }
+
+        if (tokens.empty()) {
+            continue;
+        }
+
+        pid_t pid = fork();
+
+        if (pid == 0) {
+            vector<char*> args;
+
+            for (auto& t: tokens) {
+                args.push_back(t.data());
+            }
+
+            args.push_back(nullptr);
+            execvp(args[0],args.data());
+            perror("execvp");
+            exit(1);
+        } else {
+            waitpid(pid, nullptr, 0);
+        }
+    }
+
+}
